53275f5ec27f348e1c43956889c7b64f3fdba529
Keshav Kumar <akeshav53@gmail.com>
2019-02-10 13:12:03 +0530

    a program to convert upper to lower string

---
+++ uppertolower.c
@@ -0,0 +1,11 @@
+main(){
+char str[20];
+int i;
+printf("Enter a string = ");
+gets(str);
+for(i=0;str[i];i++){
+    if(str[i]>='A' && str[i]<='Z')
+        str[i]=str[i]+32;
+}
+printf("%s",str);
+}

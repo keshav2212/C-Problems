ec14f2afde3b15d6d55204a663212aea31e084c3
Keshav Kumar <akeshav53@gmail.com>
2019-02-27 16:11:28 +0530

    program for swipe two integers without third variable

---
+++ swipe without third.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a,b;
+printf("Enter Two Values");
+scanf("%d%d",&a,&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("the two swiped numbers are :- %d and %d",a,b);
+
+       }

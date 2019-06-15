f26d7fe35d648acc45cc2cf7e27822b8e9018e49
Keshav Kumar <akeshav53@gmail.com>
2019-06-15 15:48:40 +0530

    added new function in tree

--- tree with geeksforgeeks.c
+++ tree with geeksforgeeks.c
@@ -32,15 +32,60 @@ postorder(root->left);
 postorder(root->right);
 printf("%d ",root->data);
 }
+struct tree * insert(struct tree *root,int data){
+struct tree * temp,*p,*curr;
+temp=(struct tree *)malloc(sizeof(struct tree));
+temp->data=data;
+temp->left=NULL;
+temp->right=NULL;
+if(root==NULL)
+    root=temp;
+else{
+    curr=root;
+    while(curr){
+    p=curr;
+    if(curr->data>data){
+     curr=curr->left;
+     }
+    else{
+        curr=curr->right;
+     }}
+    if(temp->data>p->data)
+        p->right=temp;
+    else
+        p->left=temp;
+}
+return root;
+    }
 int main(){
-struct tree * root=newnode(1);
-root->left=newnode(2);
-root->right=newnode(3);
-root->left->left=newnode(4);
-root->left->right=newnode(5);
-inorder(root);
-printf("\n");
-preorder(root);
-printf("\n");
-postorder(root);
+struct tree * root=NULL;
+while(1){
+printf("1. Insertion\n2. InOrder\n3. PreOrder\n4. PostOrder\n5. Exit");
+printf("\n\nEnter your choice :- ");
+int t,n;
+scanf("%d",&t);
+switch(t){
+case 1:
+    printf("Enter the data :- ");
+    scanf("%d",&n);
+    root=insert(root,n);
+    break;
+case 2:
+    inorder(root);
+    printf("\n");
+    break;
+case 3:
+    preorder(root);
+    printf("\n");
+    break;
+case 4:
+    postorder(root);
+    printf("\n");
+    break;
+case 5:
+    exit(0);
+default:
+    printf("Inavlid Input\n");
+}
+}
 }

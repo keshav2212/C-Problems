34e887e1e5d19a9c083c1187793f12eb8292560a
Keshav Kumar <akeshav53@gmail.com>
2020-07-14 23:33:18 +0530

    Codechef July Challenge Problem - 3

---
+++ july-3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int k;
+		cin>>k;
+		for(int i=0;i<8;i++){
+			for(int j=0;j<8;j++){
+				if(i==0&&j==0){
+					cout<<"O";
+					k--;
+				}
+				else if(k>0){
+					cout<<".";
+					k--;
+				}
+				else
+				cout<<"X";
+			}
+			cout<<endl;
+		}
+	}
+}

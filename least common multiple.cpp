d44d1b869e0d6003ef5a82b8af90f597b9d16797
Keshav Kumar <akeshav53@gmail.com>
2020-07-22 01:59:33 +0530

    program for least common multiple

---
+++ least common multiple.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+long long int gcd1(int a,int b){
+	long long gcd =0;
+	if(b==0)
+	return a;
+	else if(a>b)
+		gcd=gcd1(b,a%b);
+	else if(a==b)
+		gcd=a;
+	else
+		gcd=gcd1(a,b%a);
+	return gcd;
+}
+int main(){
+	long long int gcd,a,b;
+	cin>>a>>b;
+	gcd=gcd1(a,b);
+	cout<<(a*b)/gcd<<endl;
+	}
